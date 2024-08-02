0b2a09b6cbf623db226a8ebd09baf3829fe5530c
Humberto Fernández <humbertof44@gmail.com>
2024-08-01 22:18:23 -0400

    (Optional) Mock backend integration

--- src/services/mockApi.js
+++ src/services/mockApi.js
@@ -1,1 +1,10 @@
-// src/services/mockApi.js
+export const fetchPins = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { lat: 10.4806, lng: -66.9036 },
+        { lat: 10.9274, lng: -68.2738 }
+      ]);
+    }, 1000);
+  });
+};
