4c530cdc5cef492055f8dd4b085e52b8c86a3aab
Humberto Fernández <humbertof44@gmail.com>
2024-08-01 22:14:56 -0400

    Display information about a pin in a modal

--- src/components/PinDetailsModal.jsx
+++ src/components/PinDetailsModal.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
-function PinDetailsModal() {
+const PinDetailsModal = ({ show, handleClose, pin }) => {
   return (
-    <div>PinDetailsModal</div>
-  )
-}
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Detalles</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>Latitud: {pin.lat}</p>
+        <p>Longitud: {pin.lng}</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Cerrar
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
 export default PinDetailsModal;
