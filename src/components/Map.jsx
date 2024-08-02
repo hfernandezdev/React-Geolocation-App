54859082d9f4080baac47ffb09250c3199ca1df7
Humberto Fernández <humbertof44@gmail.com>
2024-08-01 22:13:13 -0400

    Add map integration

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { DefaultIcon } from '../icons';
 
-function Map() {
+const Map = ({ pins, addPin, onPinClick }) => {
+
+  const MapEvents = () => {
+    useMapEvents({
+      click: (e) => {
+        addPin(e.latlng);
+      },
+    });
+    return null;
+  };
+
   return (
-    <div>Map</div>
-  )
-}
+    <MapContainer center={[6.4238, -66.5897]} zoom={6} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {pins.map((pin, idx) => (
+        <Marker
+          key={idx}
+          position={[pin.lat, pin.lng]}
+          eventHandlers={{
+            click: () => {
+              onPinClick(pin);
+            },
+          }}
+          icon={DefaultIcon}
+        >
+          <Popup position={[pin.lat, pin.lng]}>
+            <div>
+              <h2>Pin Details</h2>
+              <p>Latitude: {pin.lat}</p>
+              <p>Longitude: {pin.lng}</p>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+      <MapEvents />
+    </MapContainer>
+  );
+};
 
 export default Map;
--- src/icons.js
+++ src/icons.js
@@ -1,1 +1,19 @@
-// src/icons.js
+import L from 'leaflet';
+
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+const DefaultIcon = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+export { DefaultIcon };
